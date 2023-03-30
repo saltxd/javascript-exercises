c7060fee6935610274f5d0b803402207a6ed96bb
marley <74526520+saltxd@users.noreply.github.com>
2023-03-30 14:43:46 -0600

    Complete section 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(string, number) {
+    if (number < 0) {
+        return "ERROR";
+     } else {
+        return string.repeat(number);
+     }
 };
 
 // Do not edit below this line
