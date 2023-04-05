89e624dfcd5a7ac4d7d0e592a86430d464cb1f8e
marley <74526520+saltxd@users.noreply.github.com>
2023-04-05 12:45:43 -0600

    Complete exercise 8, 9, 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function fibonacci(n) {
+    n = parseInt(n);
+    if (isNaN(n) || n < 0){
+        return 'OOPS';
+    } else if (n === 0 || n ===1 ){
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci (n - 2);
+    }
 };
 
 // Do not edit below this line
